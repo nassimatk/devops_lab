3fde53deabac7d32df6910254c1a03719a9e4959
Nassima <ntaikouk@gmail.com>
2026-04-21 17:32:38 +0200

    update456

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #return "Version 1 - CI/CD avec Jenkins"
-    return "Version 3 Déployée automatiquement3"
+    return "Version 4 Déployée automatiquement4"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
